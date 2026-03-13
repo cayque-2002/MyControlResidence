0b92f67d94019bdf69000ba263c3ab08e962f13a
cayque-2002 <cayque.142@gmail.com>
2026-03-13 12:40:17 -0300

    Normalização case File e ReadMe

--- src/pages/CategoriaPage.tsx
+++ src/pages/CategoriaPage.tsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify"
 import {
   FinalidadeCategoria,
   FinalidadeCategoriaTexto
-} from "../types/FinalidadeCategoria"
+} from "../types/finalidadeCategoria"
 
 import {
   getCategorias,
